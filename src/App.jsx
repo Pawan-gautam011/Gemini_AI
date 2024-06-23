50495c0f8b17450e6f3f2ecf4367c20e75a0bfd8
Pawan-gautam011 <pawangautam11op@gmail.com>
2024-06-23 23:35:29 +0545

    A bit of UI and sidebar made

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     <Sidebar/>
     </>
   )
 }
