342a1daf153bcc3ef6f0a7fc465ea6e1d2cece7a
Pawan-gautam011 <pawangautam11op@gmail.com>
2024-06-27 23:56:46 +0545

    Removing extra css

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,16 +1,24 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import run from "../config/gemini";
 
 export const Context = createContext();
 
 
 const ContextProvider = (props) =>{
-    
+
+
+    const [input, setInput] = useState("")
+    const[recentPrompt, setRecentPrompt] = useState("")
+    const [prevPrompts,setPrevPrompts] = useState([])
+    const [showResult,setShowResult] = useState(false);
+    const [loading,setLoading] = useState(false)
+    const [resultData,setResultData] = useState("")
+
     const onSent = async (prompt)=>{
         await run(prompt)
     }
 
-    onSent("WHat is react js")
+    
 
     const contextValue = {
 
